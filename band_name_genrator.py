3d97df2d5bcd51e70a84ebf6cc2baa465aad4500
Your name here <your_email@example.com>
2022-09-23 16:30:38 +0100

    first challenge project

---
+++ band_name_genrator.py
@@ -0,0 +1,4 @@
+print("Welcome to brand name generator.")
+city: str = input("What the name of the city you grew up in?\n")
+pet: str = input("What the name of your pet?\n")
+print(f"Your brand name could be {city} {pet}.")
